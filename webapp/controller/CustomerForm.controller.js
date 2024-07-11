c399bff5bd252426d6ea6b696f2acd82093a129b
arthuurfarias <arthuurfarias@hotmail.com>
2024-07-11 00:25:46 -0300

    Projeto Fiori Freestyle - 030

---
+++ webapp/controller/CustomerForm.controller.js
@@ -0,0 +1,32 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("zov.controller.CustomerForm", {
+            onInit: function () {
+                var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+
+                oRouter.getRoute("RouteToNewCustomer").attachMatched(this._onRouteMatchNew,this);
+                oRouter.getRoute("RouteToEditCustomer").attachMatched(this._onRouteMatchEdit,this);
+
+            },
+
+            onNavBack: function () {
+                const oHistory = sap.ui.core.routing.History.getInstace();
+                const sPreviousHash = oHistory.getPreviousHash();
+
+                if (sPreviousHash !== undefined) {
+                    window.history.go(-1);
+                } else {
+                    var Customer = sap.ui.core.UIComponent.getRouterFor(this);
+                    Customer.navTo("RouteView1");
+                }
+            },
+
+            _onRouteMatchNew: function(oEvent){
+                alert("Modo Criação");  
+            }
+        });
+    });
--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,12 +1,17 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ],
-function (Controller) {
-    "use strict";
+    function (Controller) {
+        "use strict";
 
-    return Controller.extend("zov.controller.View1", {
-        onInit: function () {
+        return Controller.extend("zov.controller.View1", {
+            onInit: function () {
 
-        }
+            },
+
+            onNewCustomer: function () {
+                var Customer = sap.ui.core.UIComponent.getRouterFor(this);
+                Customer.navTo("RouteToNewCustomer");
+            }
+        });
     });
-});
