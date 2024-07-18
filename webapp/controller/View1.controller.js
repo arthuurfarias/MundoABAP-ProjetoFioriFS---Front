701dffaf9205e2e0bda0ee93911e498238c72628
arthuurfarias <arthuurfarias@hotmail.com>
2024-07-17 23:47:20 -0300

    Projeto Fiori Freestyle - 034

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,10 +1,13 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "../model/formatter",
 ],
-    function (Controller) {
+    function (Controller, formatter) {
         "use strict";
 
         return Controller.extend("zov.controller.View1", {
+            formatter: formatter,
+            
             onInit: function () {
 
             },
---
+++ webapp/model/formatter.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+], 
+function () {
+    "use strict";
+
+    return {
+        formatPrice: function(sValue){
+            var oFormatOptions = {
+                groupingEnabled : true,
+                groupingSeparator: '.',
+                decimalSeparator: ',',
+                decimals: 2,
+                showMeasure: true
+            };
+            var oFloatFormat = sap.ui.core.format.NumberFormat.getFloatInstance(oFormatOptions);
+            return oFloatFormat.format(parseFloat(sValue));
+        },
+    };
+
+});
